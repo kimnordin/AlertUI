0a9d347d9784a88b736ed0657f986042d29286fd
Kim <nordinsane@gmail.com>
2024-05-06 16:16:17 +0200

    Public AlertManager Initializer
    Separate logic to Add and Display an Alert

--- Sources/AlertKit/Views/AlertManager.swift
+++ Sources/AlertKit/Views/AlertManager.swift
@@ -10,16 +10,48 @@ import SwiftUI
 public class AlertManager: ObservableObject {
     @Published var alerts = [CustomAlert]()
     
-    public func displayAlert(_ alert: CustomAlert) {
-        DispatchQueue.main.async {
-            self.alerts.append(alert)
+    public init(alerts: [CustomAlert] = []) {
+        self.alerts = alerts
+    }
+    
+    /**
+     Adds an alert to the `alerts` stack.
+     - parameter alert: The `CustomAlert` to add.
+     - parameter displayed: Whether the alert should be displayed immediately. Is `true` by default.
+     */
+    public func addAlert(_ alert: CustomAlert, displayed: Bool = true) {
+        DispatchQueue.main.async { [self] in
+            alerts.append(alert)
+            if displayed {
+                displayAlert(alert)
+            }
         }
     }
     
-    public func dismissAlert(_ alert: CustomAlert) {
-        if let index = self.alerts.firstIndex(where: { $0.id == alert.id }) {
-            DispatchQueue.main.async {
-                self.alerts.remove(at: index)
+    /**
+     Displays an alert from the `alerts` stack.
+     - parameter alert: The `CustomAlert` to display.
+     */
+    public func displayAlert(_ alert: CustomAlert) {
+        DispatchQueue.main.async { [self] in
+            if let index = alerts.firstIndex(where: { $0.id == alert.id }) {
+                alerts[index].displayed = true
+            }
+        }
+    }
+    
+    /**
+     Dismiss an alert from the `alerts` stack.
+     - parameter alert: An optional `CustomAlert` to dismiss.
+     
+     If no `CustomAlert` was specified, the first alert in the stack will be dismissed.
+     */
+    public func dismissAlert(_ alert: CustomAlert? = nil) {
+        DispatchQueue.main.async { [self] in
+            if let alert = alert, let index = alerts.firstIndex(where: { $0.id == alert.id }) {
+                alerts.remove(at: index)
+            } else {
+                alerts.removeFirst()
             }
         }
     }
