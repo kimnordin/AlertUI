890b99fee31a936bb0966fc4d17288c60002ba76
Kim <nordinsane@gmail.com>
2024-05-06 16:52:31 +0200

    Update CustomAlert and AlertCardView

--- Sources/AlertKit/Models/CustomAlert.swift
+++ Sources/AlertKit/Models/CustomAlert.swift
@@ -8,21 +8,25 @@
 import Foundation
 
 public struct AlertAction {
-    var title: String = "okayButtonTitle"
+    var title: String
     var action: (() -> Void)
 }
 
 public struct CustomAlert {
     let id = UUID()
-    var message: String = ""
-    var buttonTitle: String = ""
+    var title: String
+    var message: String
+    var buttonTitle: String
     var alertAction: AlertAction?
+    var displayed: Bool = true
     var queue: String = "main"
     
-    public init(message: String = "", buttonTitle: String = "", alertAction: AlertAction? = nil, queue: String = "main") {
+    public init(title: String, message: String, buttonTitle: String, alertAction: AlertAction? = nil, displayed: Bool = true, queue: String = "main") {
+        self.title = title
         self.message = message
         self.buttonTitle = buttonTitle
         self.alertAction = alertAction
+        self.displayed = displayed
         self.queue = queue
     }
 }
--- Sources/AlertKit/Views/AlertCardView.swift
+++ Sources/AlertKit/Views/AlertCardView.swift
@@ -12,22 +12,30 @@ public struct AlertCardView: View {
     var alert: CustomAlert
     
     public var body: some View {
-        Button(role: .cancel, action: {
-            manager.dismissAlert(alert)
-        }, label: {
-            Text(.init(alert.buttonTitle))
-        })
-        if let alertAction = alert.alertAction {
-            Button(role: .destructive, action: {
-                alertAction.action()
-            }, label: {
-                Text(.init(alertAction.title))
-            })
+        VStack(spacing: 10) {
+            Text(alert.title)
+                .bold()
+            Text(alert.message)
+            HStack {
+                Button(role: .cancel, action: {
+                    manager.dismissAlert(alert)
+                }, label: {
+                    Text(.init(alert.buttonTitle))
+                })
+                if let alertAction = alert.alertAction {
+                    Button(role: .destructive, action: {
+                        alertAction.action()
+                    }, label: {
+                        Text(.init(alertAction.title))
+                    })
+                }
+            }
         }
-    }}
+    }
+}
 
 struct AlertCardView_Previews: PreviewProvider {
     static var previews: some View {
-        AlertCardView(manager: AlertManager(), alert: CustomAlert())
+        AlertCardView(manager: AlertManager(), alert: CustomAlert(title: "Alert Title", message: "Longer Descriptive Alert Message", buttonTitle: "Okay"))
     }
 }
