ac8d5890877901688a2dae94d358d81a490c7057
Kim <nordinsane@gmail.com>
2024-05-09 15:27:48 +0200

    Alert Model Documentation

--- Sources/AlertUI/Models/Alert.swift
+++ Sources/AlertUI/Models/Alert.swift
@@ -7,11 +7,28 @@
 
 import SwiftUI
 
+/**
+ ### Alert Action
+ 
+ Encapsulates the details and behavior of an action that can be taken by a presented `Alert`.
+ Each action is represented by a button in the `AlertView`.
+ */
 public struct AlertAction {
+    /// Title of the button.
     var title: String
+    
+    /// An optional action to perform when pressing the button.
     var action: (() -> Void)?
+    
+    /// Indicates whether the action should dismiss the `Alert` it belongs to.
     var shouldDismiss: Bool
     
+    /**
+     Initializes an `AlertAction`.
+     - parameter title: The title of the button.
+     - parameter action: An optional closure that defines actions to perform when the button is pressed.
+     - parameter shouldDismiss: Whether the action should dismiss the `Alert` it belongs to. Defaults to `true`.
+     */
     public init(title: String, action: (() -> Void)? = nil, shouldDismiss: Bool = true) {
         self.title = title
         self.action = action
@@ -19,19 +36,39 @@ public struct AlertAction {
     }
 }
 
+/**
+ ### Alert
+ 
+ Represents the `Alert` details displayed by an `AlertView`.
+ */
 public struct Alert: Identifiable {
     public let id = UUID()
+    /// Title of the `Alert`.
     var title: String
+    
+    /// An optional message of the `Alert`.
     var message: String?
-    var backgroundColor: Color
+    
+    /// The primary `AlertAction` associated with the `Alert`.
     var primaryAction: AlertAction
+    
+    /// An optional secondary `AlertAction` associated with the `Alert`.
     var secondaryAction: AlertAction?
+    
+    /// The queue associated with the `Alert`.
     var queue: String = "main"
     
-    public init(title: String, message: String? = nil, backgroundColor: Color = .gray.opacity(0.5), primaryAction: AlertAction = AlertAction(title: "Okay"), secondaryAction: AlertAction? = nil, queue: String = "main") {
+    /**
+     Initializes an `Alert`.
+     - parameter title: The title of the `Alert`.
+     - parameter message: An optional message of the `Alert`.
+     - parameter primaryAction: The primary `AlertAction` associated with the `Alert`. Defaults to `AlertAction(title: "Okay")`.
+     - parameter secondaryAction: An optional secondary `AlertAction` associated with the `Alert`.
+     - parameter queue: The queue associated with the `Alert`. Defaults to `"main"`.
+     */
+    public init(title: String, message: String? = nil, primaryAction: AlertAction = AlertAction(title: "Okay"), secondaryAction: AlertAction? = nil, queue: String = "main") {
         self.title = title
         self.message = message
-        self.backgroundColor = backgroundColor
         self.primaryAction = primaryAction
         self.secondaryAction = secondaryAction
         self.queue = queue
