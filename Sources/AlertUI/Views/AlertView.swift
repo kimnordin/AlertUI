0506d28818a4c493bb36055da0b1486f167cccef
Kim <nordinsane@gmail.com>
2024-05-09 15:06:21 +0200

    Add Documentation

--- Sources/AlertUI/Views/AlertView.swift
+++ Sources/AlertUI/Views/AlertView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+/**
+ ### Alert View
+ 
+ Displays the visual representation of an individual `Alert`.
+ 
+ The `AlertView` can display a title, an optional message, and up to two action buttons.
+ */
 public struct AlertView: View {
     @ObservedObject var alertManager: AlertManager
     public var alert: Alert
     
+    /**
+     Initializes an `AlertView` with an `AlertManager` and the `Alert` content to display.
+     - parameter alertManager: An `AlertManager` to manage the displayed `Alert`. Primarily used to dismiss the referenced `Alert` when the `shouldDismiss` parameter of the `Alert` is `true`.
+     - parameter alert: The `Alert` with the content that will be displayed by the `AlertView`.
+     */
     init(_ alertManager: AlertManager, alert: Alert) {
         self.alertManager = alertManager
         self.alert = alert
@@ -59,6 +71,7 @@ public struct AlertView: View {
         .padding()
     }
 }
+
 struct AlertView_Previews: PreviewProvider {
     static var previews: some View {
         AlertView(AlertManager(), alert: Alert(title: "Alert Title", message: "Longer Descriptive Alert Message"))
